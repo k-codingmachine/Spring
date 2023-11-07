e28b933ec52560fc6264970af229348d8f44abe1
SEUNGHOONBAEK <128911668+k-codingmachine@users.noreply.github.com>
2023-11-07 11:41:30 +0900

    crud

--- MybatisTest/src/main/java/com/ezen/mybatis/prj/MainClass.java
+++ MybatisTest/src/main/java/com/ezen/mybatis/prj/MainClass.java
@@ -1,6 +1,7 @@
 package com.ezen.mybatis.prj;
 
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -22,15 +23,20 @@ public class MainClass {
 			MemberVO mVo = mapper.selectOne(00001);
 			System.out.println(mVo);
 			
-//			mVo.setId(00003);
-//			mVo.setName("조운");
+//			mVo.setId(00002);
+//			mVo.setName("미방");
 //			mVo.setPhone("111-2222-3333");
-//			mVo.setAddress("서울시 강서구");
-//			int result = mapper.insertMember(mVo);
+//			mVo.setAddress("서울시 강서구 등촌동");
+//			int result = mapper.updateMember(mVo);
 //			System.out.println("결과 : " + result);
 			
-			int result = mapper.deleteMember(00003);
-			System.out.println("결과 : " + result);
+			List<MemberVO> list = mapper.selectAll();
+			
+			for(MemberVO member : list)
+				System.out.println(member);
+			
+//			int result = mapper.deleteMember(00003);
+//			System.out.println("결과 : " + result);
 		
 		}catch (Exception e) {
 			e.printStackTrace();
--- MybatisTest/src/main/java/com/ezen/mybatis/prj/mapperInterfacce.java
+++ MybatisTest/src/main/java/com/ezen/mybatis/prj/mapperInterfacce.java
@@ -1,9 +1,14 @@
 package com.ezen.mybatis.prj;
 
+import java.util.List;
+
 public interface mapperInterfacce {
 	
 	
 	public MemberVO selectOne(int id);
 	public int insertMember(MemberVO mVo);
 	public int deleteMember(int id);
+	public int updateMember(MemberVO mVo);
+	
+	public List<MemberVO> selectAll();
 }
