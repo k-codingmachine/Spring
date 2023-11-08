227882cbd9644276aca09123fc84b04b16534c10
SEUNGHOONBAEK <128911668+k-codingmachine@users.noreply.github.com>
2023-11-08 12:31:50 +0900

    2장 end

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -1,14 +1,24 @@
 package org.zerock.controller;
 
 
+import java.nio.file.Files;
+import java.util.ArrayList;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.SampleDTO;
 import org.zerock.domain.SampleDTOList;
@@ -16,11 +26,61 @@ import org.zerock.domain.SampleDTOList;
 import lombok.extern.log4j.Log4j;
 
 @Controller
-//@RestController
+//@RestController    //(@Controller + @RequestBody(@ResponseBody))
 @RequestMapping("/sample/*")
 @Log4j
 public class SampleController {
    
+	@PostMapping("/exUploadPost")
+	public void exUploadPost(@RequestParam("files") ArrayList<MultipartFile> files) {
+	    files.forEach(file -> {
+	        log.info("------------------------");
+	        log.info("name : " + file.getOriginalFilename());
+	        log.info("size : " + file.getSize());
+	    });
+	}
+
+	
+	
+	@GetMapping("/exUpload")
+	public void exUpload() {
+	    log.info("/exUpload....");
+	}
+
+	
+	
+	@GetMapping("/ex10")
+	public ResponseEntity<String> ex10(){
+		log.info("ex10");
+		String msg = "{\"name\" : \"홍길동\"}";
+				
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Type", "application/json;charset=utf-8");
+		return new ResponseEntity<String>(msg, headers, HttpStatus.OK);
+	}
+	
+	
+	
+	@GetMapping("/ex09")
+	public  void ex09(@RequestBody SampleDTO dto) {
+		log.info("ex09.....");
+		log.info(dto.getName());
+		log.info(dto.getAge());
+	}
+	
+	
+	@GetMapping("/ex08")
+	public @ResponseBody SampleDTO ex08() {
+//	public  SampleDTO ex08() { @RestController 가 설정되어있으면 @ResponseBody를 뺄수있다.
+		log.info("ex08.....");
+		SampleDTO dto = new SampleDTO();
+		dto.setName("조조");
+		dto.setAge(20);
+		
+		return dto;
+	}
+	
+	
 	@GetMapping("/ex06/{name}/{age}")
 	public String ex06(@PathVariable("name") String name, @PathVariable("age") int age) {
 		log.info("name >> " + name);
